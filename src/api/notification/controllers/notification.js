daf6715d6220700004f041675b0f7f99e3d971e0
ArthurBoucard <arthur.boucard@hotmail.fr>
2023-10-25 14:37:39 +0200

    merge custom route with files

--- src/api/notification/controllers/notification.js
+++ src/api/notification/controllers/notification.js
@@ -6,4 +6,8 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::notification.notification');
+module.exports = createCoreController('api::notification.notification', ({strapi}) => ({
+    async postGlobal(ctx, next) {
+        return true
+    },
+}));
--- src/api/notification/routes/notification.js
+++ src/api/notification/routes/notification.js
@@ -6,4 +6,27 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::notification.notification');
+const defaultRouter = createCoreRouter('api::notification.notification');
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: 'POST',
+    path: '/notifications/global',
+    handler: 'notification.postGlobal',
+  },
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
