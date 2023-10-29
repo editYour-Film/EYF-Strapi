985f3966e3463c3e8e41c1c070e55fda9d34d5a2
Mehdi Marouani <mehdi.marouani.developer@gmail.com>
2023-10-29 23:14:27 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env(
+    "PUBLIC_URL",
+    "https://1b48b31f-5be7-4ddd-a28d-1d70627c1cfe.pub.instances.scw.cloud"
+  ),
   app: {
     keys: env.array("APP_KEYS"),
   },
