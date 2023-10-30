2d188bff4d0a530566e767a3ea39f22971bb9871
ArthurBoucard <arthur.boucard@hotmail.fr>
2023-10-30 17:33:21 +0100

    added editor specific notifications

--- src/api/notification/content-types/notification/lifecycles.js
+++ src/api/notification/content-types/notification/lifecycles.js
@@ -2,21 +2,40 @@
 
 module.exports = {
   async afterCreate(event) {
-  const { result, params: { data } } = event;
+    const { result, params: { data } } = event;
+    let userInfos = [];
 
-  if (data.type === "everyone") {
-      const userInfos = await strapi.entityService.findMany('api::user-info.user-info')
+    if (data.type === "everyone") {
+        userInfos = await strapi.entityService.findMany('api::user-info.user-info')
+    } else if (data.type === "all-editor") {
+      const users = await strapi.entityService.findMany('plugin::users-permissions.user', {
+        filters: {
+          role: {
+            name: 'editor',
+          },
+        },
+        populate: ['role']
+      });
+      const userIds = users.map(users => users.id);
 
-      if (userInfos.length > 0) {
-        const userInfosIds = userInfos.map(userInfo => userInfo.id);
-  
-        await strapi.query('api::notification.notification').update({
-          where: { id: result.id },
-          data: {
-            user: userInfosIds
-          }
-        });
-      }
+      userInfos = await strapi.entityService.findMany('api::user-info.user-info', {
+        filters: {
+          user_account: {
+            id: userIds,
+          },
+        }
+      })
+    }
+
+    if (userInfos.length > 0) {
+      const userInfosIds = userInfos.map(userInfo => userInfo.id);
+
+      await strapi.query('api::notification.notification').update({
+        where: { id: result.id },
+        data: {
+          user: userInfosIds
+        }
+      });
     }
   }
 }
