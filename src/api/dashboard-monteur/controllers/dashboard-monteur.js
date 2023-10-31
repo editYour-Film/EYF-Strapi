68d5cbc4720457df6a6833952ffb406eaa08c6c1
ArthurBoucard <arthur.boucard@hotmail.fr>
2023-10-31 12:04:57 +0100

    added new text element for dashboard

---
+++ src/api/dashboard-monteur/controllers/dashboard-monteur.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dashboard-monteur controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dashboard-monteur.dashboard-monteur');
---
+++ src/api/dashboard-monteur/routes/dashboard-monteur.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dashboard-monteur router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::dashboard-monteur.dashboard-monteur');
