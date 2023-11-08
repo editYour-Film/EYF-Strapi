1d35d9cf574769be31f6171ab023d9199580fb9e
ArthurBoucard <arthur.boucard@hotmail.fr>
2023-11-08 12:14:57 +0100

    coding style

--- src/api/editor-video/content-types/editor-video/lifecycles.js
+++ src/api/editor-video/content-types/editor-video/lifecycles.js
@@ -5,10 +5,10 @@ module.exports = {
     const { result, params: { data } } = event;
 
     await strapi.plugins["email"].services.email.send({
-    to: "admin@edityour.film",
-    from: process.env.STRAPI_ADMIN_EMAIL_SENDER,
-    subject: "Nouveau modèle ajouté",
-    text: "Un nouveau modèle a été ajouté",
+        to: "admin@edityour.film",
+        from: process.env.STRAPI_ADMIN_EMAIL_SENDER,
+        subject: "Nouveau modèle ajouté",
+        text: "Un nouveau modèle a été ajouté",
     });
   }
 }
