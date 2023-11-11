17d2ad5b5fd68c1147e423d9189f52fafde8c5ef
Mehdi Marouani <mehdi.marouani.developer@gmail.com>
2023-11-11 17:06:08 +0100

    schema change

---
+++ src/api/video-software/services/video-software.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-software service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::video-software.video-software');
