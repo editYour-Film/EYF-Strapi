191d97ba044c8b7b037b52150a222ba3369effa2
ArthurBoucard <arthur.boucard@hotmail.fr>
2023-11-21 12:31:03 +0100

    added email send when model is approved

--- src/api/editor-video/content-types/editor-video/lifecycles.js
+++ src/api/editor-video/content-types/editor-video/lifecycles.js
@@ -5,10 +5,34 @@ module.exports = {
     const { result, params: { data } } = event;
 
     await strapi.plugins["email"].services.email.send({
-        to: process.env.STRAPI_ADMIN_EMAIL_RECEIVER,
-        from: process.env.STRAPI_ADMIN_EMAIL_SENDER,
-        subject: "Nouveau modèle ajouté",
-        text: "Un nouveau modèle a été ajouté",
+      to: process.env.STRAPI_ADMIN_EMAIL_RECEIVER,
+      from: process.env.STRAPI_ADMIN_EMAIL_SENDER,
+      subject: "Nouveau modèle ajouté",
+      text: "Un nouveau modèle a été ajouté",
     });
+  },
+  async beforeUpdate(event) {
+    const params = event.params;
+  
+    const { id } = params.where;
+    const previousData = await strapi.entityService.findOne('api::editor-video.editor-video', id, {
+        populate: '*'
+    })
+
+    const previousApprove = previousData.approved;
+    const currentApprove = params.data.approved;
+
+    if (!previousApprove && currentApprove) {
+      const user = await strapi.entityService.findOne('api::user-info.user-info', previousData.user_info.id, {
+        populate: '*'
+      })
+
+      await strapi.plugins["email"].services.email.send({
+        to: user.user_account.email,
+        from: process.env.STRAPI_ADMIN_EMAIL_SENDER,
+        subject: "Votre modèle a été publié",
+        text: user.user_account.username + "\nVotre modèle a été publié",
+      });
+    }
   }
 }
